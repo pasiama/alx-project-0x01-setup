a369e3273ca78197027913e472f570858b8915bf
Asiama Phoebe <phoebeasiama6@gmail.com>
2024-12-20 21:21:56 +0000

    update

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,18 @@
+import { UserProps } from "@/interfaces";
+
+const UserCard: React.FC<UserProps> = ({ id, name, username, email }) => {
+  return (
+    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="mb-4">
+        <h2 className="text-2xl font-semibold text-gray-800">{id}</h2>
+      </div>
+      <p className="text-gray-600">{name}</p>
+      <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
+        <span>User UserName: {username}</span>
+        <span>Post Email: {email}</span>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
